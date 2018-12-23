47d41a39b466a3778daf0b3d8d424af17d897e1b
SULYXXII <SULYXXII@gmail.com>
2018-12-23 21:49:39 +0100

    First Comit
    First Comit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(ConfigurableJoint))]
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivityX = 3f;
+    [SerializeField]
+    private float lookSensitivityY = 3f;
+
+    [SerializeField]
+    private float thrusterForce = 1000f;
+
+    [SerializeField]
+    private float thrusterFuelBurnSpeed = 1f;
+    [SerializeField]
+    private float thrusterFuelRegenSpeed = 0.3f;
+    private float thrusterFuelAmount = 1f;
+
+    public float GetThrusterFuelAmount()
+    {
+        return thrusterFuelAmount;
+    }
+
+    [SerializeField]
+    private LayerMask environmentMask;
+
+    [Header("Spring settings :")]
+    [SerializeField]
+    private float jointSpring = 20f;
+    [SerializeField]
+    private float jointMaxForce = 40f;
+
+    private PlayerMotor motor;
+    private ConfigurableJoint joint;
+    private Animator animator;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+        joint = GetComponent<ConfigurableJoint>();
+        animator = GetComponent<Animator>();
+        SetJointSettings(jointSpring);
+    }
+
+    private void Update()
+    {
+        if (PauseMenu.isOn == true)
+        {
+            if (Cursor.lockState != CursorLockMode.None)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+
+            motor.Move(Vector3.zero);
+            motor.Rotate(Vector3.zero);
+            motor.RotateCamera(0f);
+            motor.ApplyThruster(Vector3.zero);
+
+            return;
+        }
+
+        if(Cursor.lockState != CursorLockMode.Locked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        RaycastHit _hit;
+        if(Physics.Raycast(transform.position, Vector3.down, out _hit, 100f, environmentMask)){
+            joint.targetPosition = new Vector3(0f, -_hit.point.y, 0f);
+        }
+        else
+        {
+            joint.targetPosition = new Vector3(0f, 0f, 0f);
+        }
+
+        // On va calculer la vélocité du mouvement du joueur en un Vecteur 3D
+        float _xMov = Input.GetAxis("Horizontal");
+        float _zMov = Input.GetAxis("Vertical");
+
+        Vector3 _movHorizontal = transform.right * _xMov;
+        Vector3 _movVertical = transform.forward * _zMov;
+
+        Vector3 _velocity = (_movHorizontal + _movVertical) * speed;
+
+        // Jouer les animations 
+        animator.SetFloat("ForwardVelocity", _zMov);
+
+        motor.Move(_velocity);
+
+        // On va calculer la rotation du joueur en un Vecteur 3D
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0, _yRot, 0) * lookSensitivityX;
+
+        motor.Rotate(_rotation);
+
+        // On va calculer la rotation de la camera en un Vecteur 3D
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        float _cameraRotationX = _xRot * lookSensitivityY;
+
+        motor.RotateCamera(_cameraRotationX);
+
+        // Calcul de la force du jetpack / thruster
+        Vector3 _thrusterForce = Vector3.zero;
+        if (Input.GetButton("Jump") && thrusterFuelAmount > 0f)
+        {
+            thrusterFuelAmount -= thrusterFuelBurnSpeed * Time.deltaTime;
+
+            if (thrusterFuelAmount >= 0.01f)
+            {
+                _thrusterForce = Vector3.up * thrusterForce;
+                SetJointSettings(0f);
+            }
+        }
+        else
+        {
+            thrusterFuelAmount += thrusterFuelRegenSpeed * Time.deltaTime;
+            SetJointSettings(jointSpring);
+        }
+
+        thrusterFuelAmount = Mathf.Clamp(thrusterFuelAmount, 0f, 1f);
+
+        // Appliquer la variable thrusterForce
+        motor.ApplyThruster(_thrusterForce);
+    }
+
+    private void SetJointSettings(float _jointSpring)
+    {
+        joint.yDrive = new JointDrive {
+            positionSpring = _jointSpring,
+            maximumForce = jointMaxForce
+        };
+    }
+}
